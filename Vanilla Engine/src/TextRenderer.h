5f7e068460444d217a66fb9583f2507ccdf559c4
cjmms <brandon.xyw@gmail.com>
2020-12-12 04:44:35 -0800

    reconstruct text renderer

---
+++ Vanilla Engine/src/TextRenderer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include "Shader.h"
+
+#include <iostream>
+#include <map>
+
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "ext.hpp"
+
+class TextRenderer
+{
+private:
+    struct Character {
+        unsigned int TextureID; // ID handle of the glyph texture
+        glm::ivec2   Size;      // Size of glyph
+        glm::ivec2   Bearing;   // Offset from baseline to left/top of glyph
+        unsigned int Advance;   // Horizontal offset to advance to next glyph
+    };
+
+    std::map<GLchar, Character> Characters;
+    unsigned int VAO, VBO;
+    unsigned int SCR_WIDTH;
+    unsigned int SCR_HEIGHT;
+
+
+public:
+	void RenderText(Shader& shader, std::string text, float x, float y, float scale, glm::vec3 color);
+    void init(unsigned int width, unsigned int height);
+    void loadText(Shader& shader, std::string fontAddress);
+};
+
